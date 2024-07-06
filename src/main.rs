cef7a6a35bfa42444dafb54a14df658cc6af9c93
Rithvik <rithvik.padma@gmail.com>
2024-07-06 13:47:00 +0530

    Counter app build

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use tracing::Level;
+
+fn main() {
+    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    let mut count = use_signal(|| 0);
+
+    rsx! {
+        div {
+            height: "100vh",
+            display: "flex",
+            flex_direction: "column",
+            justify_content: "center",
+            align_items: "center",
+            font_weight: "800",
+            font_size: "35px",
+            div{
+                "Counter: {count}"
+            }
+            div{
+                display: "flex",
+                gap: "10px",
+                padding:"10px",
+                button{
+                    onclick: move |_| count+=1,
+                    "Inc"
+                }
+                button{
+                    onclick: move |_| count-=1,
+                    "Dec"
+                }
+            }
+        }
+    }
+}
